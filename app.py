4fe15bc4e835b8bc5c34d3a7a32f2f624693934e
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2026-04-16 17:24:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import time
  
 import warnings
 warnings.filterwarnings('ignore')
-  
+   
 st.write("## Personal Fitness Tracker")
 #st.image("", use_column_width=True)
 st.write("In this WebApp you will be able to observe your predicted calories burned in your body. Pass your parameters such as `Age`, `Gender`, `BMI`, etc., into this WebApp and then you will see the predicted value of kilocalories burned.")
