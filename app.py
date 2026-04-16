5e7159f34f0cac5d120f460358087815eacd87e8
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2026-04-16 17:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split     
 from sklearn.metrics import mean_squared_error  
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor   
+from sklearn.ensemble import RandomForestRegressor    
 from sklearn import metrics  
 import time  
  
