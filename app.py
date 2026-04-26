84c5361af2cc9dafc9c2b79cdbb9781f4b2b92d4
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2026-04-26 10:56:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sn   
 import pandas as pd      
 from sklearn.model_selection import train_test_split        
-from sklearn.metrics import mean_squared_error    
+from sklearn.metrics import mean_squared_error     
 from sklearn.linear_model import LinearRegression 
 from sklearn.ensemble import RandomForestRegressor     
 from sklearn import metrics     
